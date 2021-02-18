9f500dbf4c020c57768bbf948d7d5627aa7f17b3
Oyefeso Oluwatunmise <oyefesotunmise@gmail.com>
2021-02-18 01:50:11 +0100

    Add files via upload

---
+++ precache-manifest.5b55e61a012f34b374b5e71208c298f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c72a8eea3ec0a961fe980f596b9e5cf",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.0759b1a689a166eb4abc.bundle.js"
+  },
+  {
+    "url": "runtime~main.0759b1a689a166eb4abc.bundle.js"
+  },
+  {
+    "revision": "8569bfab2372cc97452a",
+    "url": "static/css/main.7d3c682c.chunk.css"
+  },
+  {
+    "url": "vendors~main.0759b1a689a166eb4abc.bundle.js"
+  },
+  {
+    "url": "vendors~main.0759b1a689a166eb4abc.bundle.js.LICENSE.txt"
+  }
+]);
